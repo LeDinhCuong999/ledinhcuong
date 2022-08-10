1f3e6fce2ff812cd4624c8a4226deb57ea9fa929
LeDinhCuong999 <73331487+LeDinhCuong999@users.noreply.github.com>
2022-08-10 18:46:46 +0700

    up

---
+++ src/main/java/com/example/ledinhcuong/LedinhcuongApplication.java
@@ -0,0 +1,13 @@
+package com.example.ledinhcuong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedinhcuongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LedinhcuongApplication.class, args);
+    }
+
+}
